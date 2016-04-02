e6d8de8e24398cace1cead94497ce115773abc83
Aashish Agarwal <A.Agarwal@iontrading.com>
2016-04-02 20:01:23 +0530

    Validating the update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', function () {
+    return 'test commit';
+});
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
