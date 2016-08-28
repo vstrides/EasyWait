43ceb0b6aa57c9f42e58e4a9e22bfff17cb492d3
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2016-08-28 15:49:25 +0530

    Added support for token based authentication

--- app/User.php
+++ app/User.php
@@ -12,7 +12,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'api_token',
     ];
 
     /**
@@ -21,7 +21,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'api_token',
     ];
 
     public function customers()
