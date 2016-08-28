43dad513da3c55393b3ecea746033b75fbcc9e37
Aasish Agarwal <aasish.agarwal.75@gmail.com>
2016-08-28 17:06:45 +0530

    Displaying logged in user, immediately after registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-    // test update
-});
 
 /*
 |--------------------------------------------------------------------------
@@ -28,6 +13,11 @@ Route::get('/', function () {
 */
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
+
+    Route::get('/', function () {
+        return view('welcome');
+        // test update
+    });
 
     Route::get('/home', 'HomeController@index');
 
